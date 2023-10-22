d8decf409168911719749dc03784038b73aaf4a5
Trezie <ifytrez96@gmail.com>
2023-10-22 12:25:30 +0100

    My first commit

---
+++ src/data/aboutMeData.js
@@ -0,0 +1,10 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio: "Ifeoma is a software engineer resident in Lagos, I also actively serve as a mentor for She Code Africa, specializing in guiding aspiring FrontEnd Web Developers.",
+	},
+	{
+		id: 2,
+		bio: "Beyond my dedication to technology, I am also a pharmacist and a passionate advocate for public health. I firmly believe that leveraging technology is pivotal in advancing healthcare and ensuring its accessibility to all individuals. When I am not immersed in the activities stated above, I find solace in indulging in life's simple pleasures. Reading captivating books, watching thought-provoking movies, and cherishing invaluable moments with loved ones are among the many ways I find joy outside of my professional pursuits.",
+	},
+];
---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+// Import images
+import WebImage1 from '../images/web-project-1.jpg';
+import Disneyweb from '../images/disneyweb.png';
+import Teslaweb from '../images/teslaweb.png';
+import Amazon from '../images/amazonweb.png';
+import UIImage1 from '../images/ui-project-1.jpg';
+import Techweb from '../images/techweb.png';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Disney Web App',
+		category: 'Web Application',
+		img: Disneyweb,
+		ProjectHeader: {
+			title: 'A disney web app',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'Amazon Web App',
+		category: 'Web Application',
+		img: Amazon,
+	},
+	{
+		id: 3,
+		title: 'Project Management UI',
+		category: 'Web Application',
+		img: UIImage1,
+	},
+	{
+		id: 4,
+		title: 'Tesla Web',
+		category: 'Web Application',
+		img: Teslaweb,
+	},
+	{
+		id: 5,
+		title: 'Tech Education Platform',
+		category: 'Web Application',
+		img: Techweb,
+	},
+	{
+		id: 6,
+		title: 'Apple Design System',
+		category: 'Web Application',
+		img: WebImage1,
+	},
+];
